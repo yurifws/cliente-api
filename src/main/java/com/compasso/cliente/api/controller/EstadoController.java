eb1769596c014cd4135ed0cec768d700b91bce07
yurifws <yurifws@gmail.com>
2020-02-11 14:59:15 -0300

    Ajuste Estado controller para utilizar os DTOs ao inves de utilizar as classes Domain

--- src/main/java/com/compasso/cliente/api/controller/EstadoController.java
+++ src/main/java/com/compasso/cliente/api/controller/EstadoController.java
@@ -2,11 +2,23 @@ package com.compasso.cliente.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.compasso.cliente.api.assembler.EstadoAssembler;
+import com.compasso.cliente.api.model.EstadoModel;
+import com.compasso.cliente.api.model.input.EstadoInput;
 import com.compasso.cliente.domain.model.Estado;
 import com.compasso.cliente.domain.service.EstadoService;
 
@@ -17,9 +29,37 @@ public class EstadoController {
 	@Autowired
 	private EstadoService estadoService;
 	
+	@Autowired
+	private EstadoAssembler estadoAssembler;
+	
 	@GetMapping
-	public List<Estado> listar(){
-		return estadoService.listar();
+	public List<EstadoModel> listar(){
+		return estadoAssembler.toCollectionModel(estadoService.listar());
+	}
+	
+	@GetMapping("/{id}")
+	public EstadoModel buscar(@PathVariable Long id){
+		return estadoAssembler.toModel(estadoService.buscar(id));
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Estado buscar(@RequestBody @Valid EstadoInput estadoInput){
+		return estadoService.salvar(estadoAssembler.toDomainObject(estadoInput));
+	}
+	
+	@PutMapping("/{id}")
+	public Estado buscar(@PathVariable Long id, @RequestBody @Valid EstadoInput estadoInput){
+		Estado estadoAtual = estadoService.buscar(id);
+		estadoAssembler.copyToDomainObject(estadoInput, estadoAtual);
+		return estadoService.salvar(estadoAtual);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id){
+		Estado estadoAtual = estadoService.buscar(id);
+		estadoService.remover(estadoAtual.getId());
 	}
 
 }
