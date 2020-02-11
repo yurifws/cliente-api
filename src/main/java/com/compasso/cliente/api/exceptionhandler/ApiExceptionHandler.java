69bd5a5fb4eb75a95f30d121b6a6939c63d84606
yurifws <yurifws@gmail.com>
2020-02-11 15:20:30 -0300

    Ajuste o ApiExceptionHandler

--- src/main/java/com/compasso/cliente/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/compasso/cliente/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,9 +1,74 @@
 package com.compasso.cliente.api.exceptionhandler;
 
+import java.time.OffsetDateTime;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.compasso.cliente.domain.exception.EntidadeNaoEncontradaException;
+import com.compasso.cliente.domain.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+	public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = 
+			"Ocorreu um erro interno inesperado no sistema. Tente novamente e se "
+			+ "o problema persistir, entre em contato com o administrador do sistema.";
+	
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
+			WebRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+		String detail = ex.getMessage();
+
+		Problem problem = createProblemBuilder(status, problemType, detail, detail).build();
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> handleNegocioException(NegocioException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+		String detail = ex.getMessage();
+
+		Problem problem = createProblemBuilder(status, problemType, detail, detail).build();
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		if (body == null) {
+			body = Problem.builder()
+					.status(status.value())
+					.timestamp(OffsetDateTime.now())
+					.title(status.getReasonPhrase())
+					.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL).build();
+		} else if (body instanceof String) {
+			body = Problem.builder()
+					.status(status.value())
+					.timestamp(OffsetDateTime.now())
+					.title((String) body)
+					.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL).build();
+		}
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+	
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail, String userMessage) {
+		return Problem.builder()
+				.status(status.value())
+				.timestamp(OffsetDateTime.now())
+				.type(problemType.getUri())
+				.title(problemType.getTitle())
+				.detail(detail)
+				.userMessage(userMessage);
+	}
+	
 }
