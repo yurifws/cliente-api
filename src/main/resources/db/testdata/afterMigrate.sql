f656ef38012650219e328b7546c77ea312f2a39a
yurifws <yurifws@gmail.com>
2020-02-11 14:26:47 -0300

    Criação e geração de massa da tabela Estado  por migration

---
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -0,0 +1,10 @@
+
+#set sql_safe_updates = 0;
+#set foreign_key_checks = 0;
+
+delete from tb_estado;
+
+insert into tb_estado (id, nome) values (1, 'Pernambuco');
+insert into tb_estado (id, nome) values (2, 'São Paulo');
+insert into tb_estado (id, nome) values (3, 'Acre');
+insert into tb_estado (id, nome) values (4, 'Amapá');
