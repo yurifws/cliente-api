fa1fb460773111447ca0391da55a0e79bc1b9603
yurifws <yurifws@gmail.com>
2020-02-11 15:09:21 -0300

    Iniciando a implementacao do ApiExceptionHandler com o formato de problemas no corpo de respostas

---
+++ src/main/java/com/compasso/cliente/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,18 @@
+package com.compasso.cliente.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
+	
+	private String uri;
+	private String title;
+	
+	private ProblemType(String path, String title) {
+		this.uri = String.format("https://cliente-api.com.br%s", path);
+		this.title = title;
+	}
+}
