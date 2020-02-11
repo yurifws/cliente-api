00d7874d492539280625926bc4c61b915fe1aadf
yurifws <yurifws@gmail.com>
2020-02-11 11:15:54 -0300

    Ajuste configuracao do banco e classes domain

--- src/main/java/com/compasso/cliente/domain/model/Cliente.java
+++ src/main/java/com/compasso/cliente/domain/model/Cliente.java
@@ -1,5 +1,44 @@
 package com.compasso.cliente.domain.model;
 
-public class Cliente {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String sexo;
+	
+	@Column(nullable = false)
+	private LocalDate dataNascimento;
+	
+	@Column(nullable = false)
+	private int idade;
+	
+	@ManyToOne
+	@JoinColumn(name = "cidade_id", nullable = false)
+	private Cidade cidade;
+	
 }
