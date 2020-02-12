150369e556ea699c03cc9fb9b002a5c21f002f53
yurifws <yurifws@gmail.com>
2020-02-12 02:55:58 -0300

    Ajuste Cliente model e Cidade model

--- src/main/java/com/compasso/cliente/api/model/CidadeModel.java
+++ src/main/java/com/compasso/cliente/api/model/CidadeModel.java
@@ -1,9 +1,5 @@
 package com.compasso.cliente.api.model;
 
-import java.time.LocalDate;
-
-import com.compasso.cliente.domain.model.Cidade;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +9,6 @@ public class CidadeModel {
 	
 	private Long id;
 	private String nome;
-	private char sexo;
-	private LocalDate dataNascimento;
-	private int idade;
-	private Cidade cidade;
+	private EstadoModel estado;
 
 }
