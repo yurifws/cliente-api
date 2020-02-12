f4f8864ce40026572e78a1e24ccd9a559e53bad6
yurifws <yurifws@gmail.com>
2020-02-12 03:03:41 -0300

    Ajuste Cliente Input

--- src/main/java/com/compasso/cliente/api/model/input/ClienteInput.java
+++ src/main/java/com/compasso/cliente/api/model/input/ClienteInput.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,18 +12,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ClienteInput {
-	
+
 	@NotBlank
 	private String nome;
-	
+
 	@NotBlank
-	private char sexo;
-	
-	@Pattern(regexp = "yyyy-MM-dd")
+	private String sexo;
+
 	private LocalDate dataNascimento;
-	
+
 	private int idade;
-	
+
 	@Valid
 	@NotNull
 	private CidadeIdInput cidade;
